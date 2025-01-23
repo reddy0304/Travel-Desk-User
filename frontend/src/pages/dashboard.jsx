195ba2a03f74007bb78a6fe41b18b22adfc2c90a
Hemanth <matavalam.hemanth@jindalstainless.com>
2025-01-23 10:49:04 +0530

    updated the dropdowns of enter destination,location

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -63,6 +63,25 @@ const Form = styled.div`
       font-size: 0.9rem;
     }
   }
+  
+  select {
+    padding: 10px;
+    font-size: 1rem;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+
+    @media (max-width: 768px) {
+      font-size: 0.9rem;
+    }
+  }
+   label {
+    font-size: 1rem;
+    color: #555;
+
+    @media (max-width: 768px) {
+      font-size: 0.9rem;
+    }
+  }
 
   button {
     padding: 12px;
@@ -153,8 +172,36 @@ const Homepage = () => {
         <h1>Request a ride </h1>
         <p>Add your trip details, hop in, and go.</p>
         <Form>
-          <input type="text" placeholder="Enter location" />
-          <input type="text" placeholder="Enter destination" />
+          <label htmlFor="location">Select Location:</label>
+          <select id="location" name="location">
+            <option value="club-house">Club house, JSL</option>
+            <option value="power-plant">Power Plant, JSL</option>
+            <option value="shakti-bhavan">Shakti Bhavan, JSL</option>
+            <option value="crm">CRM, JSL</option>
+            <option value="store-1">Store 1, JSL</option>
+            <option value="hsm">HSM, JSL</option>
+            <option value="quality-house">Quality House, JSL</option>
+            <option value="sms-lab">SMS_LAB office, JSL</option>
+            <option value="get-hostel">Get Hostel, JSL</option>
+            <option value="project-store">Project store, JSL</option>
+            <option value="jigging-plant">Jigging Plant, JSL</option>
+            <option value="lime-plant">Lime Plant, JSL</option>
+          </select>
+          <label htmlFor="destination">Select Destination:</label>
+          <select id="destination" name="destination">
+            <option value="club-house">Club house, JSL</option>
+            <option value="power-plant">Power Plant, JSL</option>
+            <option value="shakti-bhavan">Shakti Bhavan, JSL</option>
+            <option value="crm">CRM, JSL</option>
+            <option value="store-1">Store 1, JSL</option>
+            <option value="hsm">HSM, JSL</option>
+            <option value="quality-house">Quality House, JSL</option>
+            <option value="sms-lab">SMS_LAB office, JSL</option>
+            <option value="get-hostel">Get Hostel, JSL</option>
+            <option value="project-store">Project store, JSL</option>
+            <option value="jigging-plant">Jigging Plant, JSL</option>
+            <option value="lime-plant">Lime Plant, JSL</option>
+          </select>
           <ScheduleFields>
             <label htmlFor="ride-date">Select Date:</label>
             <input type="date" id="ride-date" />
